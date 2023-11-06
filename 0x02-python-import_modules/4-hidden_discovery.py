00b5a83dfc3ca2fc07cf36c41ad0d9807e2cdab9
= <abdoahmedsayed704@gmail.com>
2023-11-06 18:32:57 +0200

    prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    """Print all the hidden directories"""
     for count in dir(hidden_4):
         if count[-2:] != "__":
             print(count)
