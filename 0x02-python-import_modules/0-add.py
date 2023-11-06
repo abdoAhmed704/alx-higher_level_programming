76d39b2995fc5bb4776eb4a5b08be4d004f78893
= <abdoahmedsayed704@gmail.com>
2023-11-05 18:03:36 -0800

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-from add_0 import add
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __main__ == "__name__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
