a7c0f2020aed80e9458b83cdcbd29dec92439568
= <abdoahmedsayed704@gmail.com>
2023-11-23 21:33:06 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    test_list = my_list
-    test_list.reverse()
-    for idx in test_list:
+    my_list.reverse()
+    for idx in my_list:
         print("{:d}".format(idx))
