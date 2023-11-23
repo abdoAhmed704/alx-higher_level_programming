29b4111bcbb66f8c9ae21379df1254c5e57aae4e
= <abdoahmedsayed704@gmail.com>
2023-11-23 21:25:54 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if (idx < 0) or (idx + 1 > length):
-        return None
+        return my_list
     in_my_list = my_list
     in_my_list[idx] = element
     return in_my_list
