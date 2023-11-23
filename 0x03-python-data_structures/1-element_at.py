3320f9d10b0f4ad93b81deafa8583535985bdc72
= <abdoahmedsayed704@gmail.com>
2023-11-23 21:06:49 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     length = len(my_list)
-    if (idx < 0) or (idx > length + 1):
+    if (idx < 0) or (idx + 1 > length):
         return None
     return my_list[idx]
