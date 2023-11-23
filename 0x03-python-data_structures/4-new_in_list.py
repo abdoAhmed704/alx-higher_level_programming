aa1a96d0338b0c0e090f0860da87a1ed07eb1fef
= <abdoahmedsayed704@gmail.com>
2023-11-23 21:46:10 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
     test_list = my_list.copy()
     test_list[idx] = element
     return test_list
