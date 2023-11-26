6b5053f521c91de657abf97eb944861e317ccacc
= <abdoahmedsayed704@gmail.com>
2023-11-26 19:11:57 +0200

    (•̀ᴗ•́ )

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 0:
+        return None
+    bigger = my_list[0]
+    for idx in my_list:
+        if idx > bigger:
+            bigger = idx
+    return bigger
