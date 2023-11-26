91677c8971f1703ca22de4a38e1642d1e9c37d08
= <abdoahmedsayed704@gmail.com>
2023-11-26 18:04:20 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for i in matrix:
-        for j in i:
-            print("{:d} ".format(j), end="")
-        print("")
+    for small_matrix in matrix:
+        if len(small_matrix) == 0:
+            print()
+        for i in range(len(small_matrix)):
+            print("{:d}".format(small_matrix[i]),
+                  end="\n" if i == len(small_matrix) - 1 else " ")
