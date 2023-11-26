d31a6c226165d912e71303afa7916de08f32fd3d
= <abdoahmedsayed704@gmail.com>
2023-11-26 17:48:07 +0200

    (•̀ᴗ•́ )

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for i in matrix:
         for j in i:
             print("{:d} ".format(j), end="")
         print("")
+
