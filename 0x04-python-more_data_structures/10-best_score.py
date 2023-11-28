06cc74741d94a0d139adb54dd99a589e4d6490e9
= <abdoahmedsayed704@gmail.com>
2023-11-28 19:19:38 +0200

    (•̀ᴗ•́ )

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     max_val = 0
     max_key = None
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    sum = 0
+    n = 0
+    Roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        n = Romans_n[i]
+        sum += n if sum < n * 5 else -n
+    return sum
