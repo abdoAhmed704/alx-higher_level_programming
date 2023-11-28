0821cf8ccb9d7932de81e69445badac29e57dcf0
= <abdoahmedsayed704@gmail.com>
2023-11-28 17:02:03 +0200

    (•̀ᴗ•́ )

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    max_val = 0
+    max_key = None
+    for k, v in a_dictionary.items():
+        if max_val < v:
+            max_val = v
+            max_key = k
+    return max_key
