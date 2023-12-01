43c5b6b854e15a0ec47c1ad6d243cfa24e931be0
= <abdoahmedsayed704@gmail.com>
2023-12-01 19:30:55 +0200

    ☻

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,8 +40,7 @@ class Square:
         if self.__size == 0:
             print()
             return
-        if self.position[1]:
-            print()
+        [print("") for i in range(0, self.__position[1])]
         for i in range(self.__size):
             print(" " * self.position[0], end="")
             for j in range(self.__size):
