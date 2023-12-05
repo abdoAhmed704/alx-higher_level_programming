9cb592aa46e9252e8d3824f0809b9ff4b7e7601d
= <abdoahmedsayed704@gmail.com>
2023-12-05 20:49:30 +0200

    😊

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for adding integer method."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: first integer.
+        b: second integer
+
+    Raises:
+        TypeError: if a and b aren't int or float.
+
+    Returns:
+        The sum
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
