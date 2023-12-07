eab00c82c0c4cca8bc1f690ff495b7ed44997dac
= <abdoahmedsayed704@gmail.com>
2023-12-07 16:06:18 +0200

    😊

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return Rectangle(size, size)
+        return cls(size, size)
 
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
