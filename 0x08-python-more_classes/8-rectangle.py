ba015fb61a286261f9730312ade2484ee9b0a2b4
= <abdoahmedsayed704@gmail.com>
2023-12-07 15:46:23 +0200

    😊

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -58,7 +58,6 @@ class Rectangle:
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)
 
-    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
