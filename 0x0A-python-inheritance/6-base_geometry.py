3881d5188726f9760b1ff92949e960a12ee33ed1
= <abdoahmedsayed704@gmail.com>
2023-12-11 12:34:00 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+=======================
+module for BaseGeometry
+=======================
+"""
+
+
+class BaseGeometry:
+    """
+    Base Class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
