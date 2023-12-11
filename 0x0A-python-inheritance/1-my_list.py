efbd55930c119d72ad3ac8769ef37d5f7cbe57cc
= <abdoahmedsayed704@gmail.com>
2023-12-11 11:49:31 +0200

    Hi inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
 ===========================
-Module
+Module with class MyList
 ===========================
 """
 
 
 class MyList(list):
     """Class with method print_sorted"""
+    pass
 
     def print_sorted(self):
         """Methot that sorted a list"""
