df22a0f2d3d0270361b80584c9fb73eacc0f902e
= <abdoahmedsayed704@gmail.com>
2023-12-11 12:10:20 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+================================
+module to know if isinstance
+================================
+"""
+
+
+def is_same_class(obj, a_class):
+    """Method that return True if an object is an instance of a class"""
+
+    return type(obj) == a_class
