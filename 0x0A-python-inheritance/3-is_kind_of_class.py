8a1ede3372e04e70783e72b518381986f4157026
= <abdoahmedsayed704@gmail.com>
2023-12-11 12:11:44 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+================================
+module with method is_same_class
+================================
+"""
+
+
+def is_same_class(obj, a_class):
+    """Method that return True if an object is an instance of a class"""
+
+    return True if isinstance(obj, a_class) else False
