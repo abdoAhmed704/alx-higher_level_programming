87dda380ba09c6d8759b9f961480b3d92b38edab
= <abdoahmedsayed704@gmail.com>
2023-12-12 21:20:50 +0200

    Hi inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module for a Derived Class"""
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
