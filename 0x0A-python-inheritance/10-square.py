588a4ced1dc0c02c773ca005bc340ce207361bf6
= <abdoahmedsayed704@gmail.com>
2023-12-12 21:44:42 +0200

    Hi inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -26,3 +26,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
