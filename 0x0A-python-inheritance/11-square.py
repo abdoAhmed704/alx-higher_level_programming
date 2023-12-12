cd0ee1c999d411af9c872ae1dfeab60c03c0b4a2
= <abdoahmedsayed704@gmail.com>
2023-12-12 21:48:40 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for a Derived Class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square (drived) class"""
+    def __init__(self, size):
+        """Constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
