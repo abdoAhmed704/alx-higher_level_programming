847b66cc27e28878912898f838b81742804435b6
= <abdoahmedsayed704@gmail.com>
2023-12-12 15:00:39 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for a Derived Class"""
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A derived clase"""
+    def __init__(self, width, height):
+        """Constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
