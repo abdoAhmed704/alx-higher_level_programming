538c6c84ee986fbeefbdf6fc9a551cde57bb622c
= <abdoahmedsayed704@gmail.com>
2023-12-12 21:33:07 +0200

    Hi inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module for a Derived Class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A Rectangle (derived) class"""
+    def __init__(self, width, height):
+        """Constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """A Square (drived) class"""
+    def __init__(self, size):
+        """Constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
