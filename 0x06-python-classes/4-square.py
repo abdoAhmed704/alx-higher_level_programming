5fe5d4fdd6ab0bbd464613cc21dc349cc7487b48
Abdulrahman Ahmed sayed <132397042+abdoAhmed704@users.noreply.github.com>
2024-01-29 07:31:10 +0200

    Update 4-square.py
    done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,6 +19,7 @@ class Square:
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
