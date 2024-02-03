bd786fd32797c77f3cc92170df24d6b8abc67b50
= <abdoahmedsayed704@gmail.com>
2024-02-03 11:58:58 +0200

    Done..

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 my_list = []
 text_file = "add_item.json"
-
+added = list(argv[1:])
 try:
     my_list = load_from_json_file(text_file)
 except Exception:
     my_list = []
-
-for indx in argv[1:]:
-    my_list.append(indx)
+my_list.extend(added)
 save_to_json_file(my_list, text_file)
