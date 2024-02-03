2ffc04a0148dc25186ab010b12afc5787afd9f96
= <abdoahmedsayed704@gmail.com>
2024-02-03 10:35:45 +0200

    Done..

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""read from file"""
 
 def read_file(filename=""):
+    """read from file"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
