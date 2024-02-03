d39868e8bf8986321efa59eadc904d8da4c2d14b
= <abdoahmedsayed704@gmail.com>
2024-02-03 11:55:35 +0200

    Done..

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-#script that adds all arguments to a Python list
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+# script that adds all arguments to a Python list
 import json
 from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 my_list = []
 text_file = "add_item.json"
 
 try:
     my_list = load_from_json_file(text_file)
-except:
+except Exception:
     my_list = []
 
 for indx in argv[1:]:
