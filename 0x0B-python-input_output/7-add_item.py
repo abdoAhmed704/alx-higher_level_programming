0a1b4890d742de3026131ccc8f21ece851dfc1f6
= <abdoahmedsayed704@gmail.com>
2024-02-03 12:00:31 +0200

    Done..

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# script that adds all arguments to a Python list
+"""script that adds all arguments to a Python list"""
+
 import json
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-my_list = []
+
 text_file = "add_item.json"
 added = list(argv[1:])
 try:
