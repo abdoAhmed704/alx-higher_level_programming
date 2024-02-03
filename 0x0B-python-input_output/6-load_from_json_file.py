9cc8a91bcc04d879ecfb0f33bbba2884403e128e
= <abdoahmedsayed704@gmail.com>
2024-02-03 11:23:46 +0200

    Done..

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""to_json_string"""
+
+
+def load_from_json_file(filename):
+    """to_json_string"""
+    import json
+
+    with open(filename, "r") as f:
+        content = f.read()
+        return json.load(content)
