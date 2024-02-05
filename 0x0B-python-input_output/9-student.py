81ed987b515bfc24d87a404304d93ea7d75774ea
= <abdoahmedsayed704@gmail.com>
2024-02-05 09:48:41 +0200

    Done..

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,14 @@
-ii
+#!/usr/bin/python3
+"""a class"""
+
+
+class Student:
+    """a class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
