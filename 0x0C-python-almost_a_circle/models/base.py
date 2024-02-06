231d68a247480c3d6fd46a9a7572dd402271fc2d
= <abdoahmedsayed704@gmail.com>
2024-02-06 17:08:10 +0200

    Done..

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """to_json_string"""
         return json.dumps(list_dictionaries) if not None else "[]"
