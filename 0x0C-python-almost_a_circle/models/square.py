6ae70744ca8f7603fc0c7227bac6dbe0c6a96dc0
= <abdoahmedsayed704@gmail.com>
2024-02-06 16:26:23 +0200

    Done..

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
-"""Square class"""
+""" Module that contains class Square,
+inheritance of class Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    """ Class Rectangle """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes instances """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        x = f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
-        return x
+        """ str special method """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
 
     @property
     def size(self):
-        """return the size"""
+        """ Getter size """
         return self.width
 
     @size.setter
-    def size(self, val):
-        """setting the size"""
-        self.width = val
-        self.height = val
+    def size(self, value):
+        """ Setter size """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
 
     def update(self, *args, **kwargs):
-        if args is not None and len(args) != 0:
+        """ update method """
+        if args is not None and len(args) is not 0:
             list_atr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
                 if list_atr[i] == 'size':
@@ -39,3 +58,16 @@ class Square(Rectangle):
                     setattr(self, 'height', value)
                 else:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns a dictionary with attributes """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
