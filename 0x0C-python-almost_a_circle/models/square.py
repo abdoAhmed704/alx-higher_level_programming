e4a7ad7df9bf3aecc5df0d38cc71cfc9eeb9eff5
= <abdoahmedsayed704@gmail.com>
2024-02-06 15:20:43 +0200

    Done..

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,9 +4,8 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width, height)
-        self.width = self.height = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        x = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        x = f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
         return x
