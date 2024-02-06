40a6d403a0ecce756fbbececa6a9223e83ec8b5c
= <abdoahmedsayed704@gmail.com>
2024-02-06 15:23:45 +0200

    Done..

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
