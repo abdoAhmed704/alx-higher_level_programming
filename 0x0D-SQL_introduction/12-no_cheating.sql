a0c16fcce32b82d268152a51118e145e06f3583c
Abdulrahman Ahmed sayed <132397042+abdoAhmed704@users.noreply.github.com>
2024-02-15 00:14:18 +0200

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- updates data
 -- Execute: cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-UPDATE `second_table` SET `score`=10 WHERE `second_table`.`name`="Bob";
+UPDATE second_table SET score=10 WHERE name='Bob';
