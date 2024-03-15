5abc17aaa14e5cffa294f85c4070fd5b1ebbb545
= <abdoahmedsayed704@gmail.com>
2024-03-15 21:03:54 +0200

    Done..

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- LEFT JOIN
-SELECT cities.id, cities.name, states.name FROM cities left join states on states.id = cities.state_id ORDER BY cities.id;
+-- AHA
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states on states.id = cities.state_id ORDER BY cities.id;
