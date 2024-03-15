7aed2e55925f1fe97992cf5e713a223591dae660
= <abdoahmedsayed704@gmail.com>
2024-03-15 21:01:43 +0200

    Done..

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,2 @@
 -- LEFT JOIN
-SELECT cities.id, cities.name, states.name
-FROM cities c left join states s
-on s.id = c.state_id;
+SELECT cities.id, cities.name, states.name FROM cities left join states on states.id = cities.state_id;
