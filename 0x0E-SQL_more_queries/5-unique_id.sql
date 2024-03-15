3f923bee3c2b2c4601ccdf5ab9ed09e11c11bc87
= <abdoahmedsayed704@gmail.com>
2024-03-15 16:48:23 +0200

    Done..

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates the table unique_id on your MySQL server
 
-CREATE TABLE IF NOT EXISTS unique_id (id INT default value 1 unique, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT default 1 unique, name VARCHAR(256));
