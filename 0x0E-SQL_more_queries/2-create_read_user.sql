ab98db2abe560efdf10b9b284b5fc3b6aa23300a
= <abdoahmedsayed704@gmail.com>
2024-03-15 16:39:09 +0200

    Done..

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create a second user 
+
+CREATE DATABASE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
