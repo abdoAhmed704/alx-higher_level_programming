09c55c5e98a0f63ac9e7b87ea5d7f3bb017a6940
= <abdoahmedsayed704@gmail.com>
2024-03-17 21:13:24 +0200

    Done..

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    curs = db.cursor()
+    curs.execute("""SELECT * FROM states WHERE name LIKE BINARY %s
+                    ORDER BY states.id""", (argv[4]))
+    data = curs.fetchall()
+    for row in data:
+        print(row)s
+    curs.close()
+    db.close()
