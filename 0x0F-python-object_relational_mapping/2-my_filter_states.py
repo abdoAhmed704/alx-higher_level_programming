8da66e3df32618991418d66988aee6776a9ad583
= <abdoahmedsayed704@gmail.com>
2024-03-17 16:25:52 +0200

    Done..

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
+    curr.execute("""SELECT * FROM states WHERE name='{}'
+                 ORDER BY states.id""".format(sys.argv[4]))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()
