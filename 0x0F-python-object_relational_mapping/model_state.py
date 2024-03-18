9147ee5200bce7241585df0e1b0b63a0cf0b0302
= <abdoahmedsayed704@gmail.com>
2024-03-18 13:18:59 +0200

    Done..

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,15 +3,13 @@
 Start link class to table in database
 """
 
-import sys
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Base """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
