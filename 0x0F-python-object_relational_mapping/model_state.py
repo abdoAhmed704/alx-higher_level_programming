4c2c3c9a95ccbe2f5720de728e2901983b13e1f7
= <abdoahmedsayed704@gmail.com>
2024-03-18 13:14:59 +0200

    Done..

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-Start link class to table in database
-"""
+""" Defines State ORM object"""
 
-import sys
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_city import City
 
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    """State ORM class"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
