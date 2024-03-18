1ef41572c1d7c40cb9d30716cf7a69a9e6725d21
= <abdoahmedsayed704@gmail.com>
2024-03-18 13:07:23 +0200

    Done..

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+CREATE TABLE FROM Py
 """
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
-
+Base = declarative_base()
 
 class State(Base):
-    """
-    Class with id and name attributes of each state
-    """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
