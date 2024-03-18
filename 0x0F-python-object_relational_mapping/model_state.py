d30c6ebc82d781cdc2b98624ad83ac35c0bb9302
= <abdoahmedsayed704@gmail.com>
2024-03-18 13:18:11 +0200

    Done..

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,5 +14,5 @@ class State(Base):
     """Base """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
