2f35ca5f78822d69f15985a419a995f663e420d2
= <abdoahmedsayed704@gmail.com>
2024-07-12 04:20:45 +0300

    Done..

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 # script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
 
-curl -s "$1" | wc -c
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
