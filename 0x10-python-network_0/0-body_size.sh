b8ed0ef4343d90a0b96e8d22553325abeca103b8
= <abdoahmedsayed704@gmail.com>
2024-07-12 04:26:55 +0300

    Done..

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Check if the script is given exactly one argument
-curl -s -o /dev/null -w '%{size_download}' "$url"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
