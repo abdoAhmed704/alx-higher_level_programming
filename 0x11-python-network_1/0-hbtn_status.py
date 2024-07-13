463c38f6a1a71f94402121b720c1c673c52db673
= <abdoahmedsayed704@gmail.com>
2024-07-13 04:18:48 +0300

    Done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     from urllib import request
     with request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         data = res.read()
-        print('Body response:$')
-        print(f'\t- type: {type(data)}$')
-        print(f'\t- content: {data}$')
-        print(f'\t- utf8 content: {data.decode("utf-8")}$')
+        print('Body response:')
+        print(f'\t- type: {type(data)}')
+        print(f'\t- content: {data}')
+        print(f'\t- utf8 content: {data.decode("utf-8")}')
