9f01aa27e7363819f9781ee30d2c73712efcd5de
= <abdoahmedsayed704@gmail.com>
2024-07-13 15:01:54 +0300

    Done

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as res:
             body = res.read().decode("utf-8")
+            print(body)
     except urllib.error.HTTPError as e:
         print(f'Error code: {e.code}')
