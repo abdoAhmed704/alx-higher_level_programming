159789be06cf9e84d0f1889139cb8d4b8b86ef65
= <abdoahmedsayed704@gmail.com>
2024-07-13 16:10:11 +0300

    Done

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetching URL"""
+
+
+if __name__ == "__main__":
+    import requests
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.content}")
