be750de505e2bd276a011548fe5847ea3b7f442f
= <abdoahmedsayed704@gmail.com>
2024-07-13 16:12:29 +0300

    Done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print(f"\t- type: {type(res.text)}")
-    print(f"\t- content: {res.content}")
+    print(f"\t- content: {res.text}")
